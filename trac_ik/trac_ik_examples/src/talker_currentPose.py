8c0e4e4047248d72e6cf781bc003279cf82b97c3
Naoto Komeno <naoteen0315@gmail.com>
2022-02-16 16:26:08 +0900

    Add files via upload

--- trac_ik/trac_ik_examples/src/talker_currentPose.py
+++ trac_ik/trac_ik_examples/src/talker_currentPose.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python
-import talker_IK_target_pose
+#!/usr/bin/env python3
+import moveit_commander
 import rospy
 import math
 import sys    
 from geometry_msgs.msg import PoseStamped
 
-
+"""
+roslaunch ur_robot_driver ur5_bringup.launch robot_ip:=192.168.11.6
+roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch sim:=false
+rosbag record /ur_current_cartesian_pose
+"""
 if __name__ == '__main__':
+    robot = moveit_commander.RobotCommander()
+    mani = moveit_commander.MoveGroupCommander("manipulator")
     rospy.init_node('current_pose', anonymous=True)
     pub = rospy.Publisher('ur_current_cartesian_pose', PoseStamped, queue_size=10)
-    ur = talker_IK_target_pose.targetTalker()
     rate = rospy.Rate(100)
 
     while not rospy.is_shutdown():
-        pose = ur.mani.get_current_pose()
+        pose = mani.get_current_pose()
         pub.publish(pose)
         print(pose)
         rate.sleep()
